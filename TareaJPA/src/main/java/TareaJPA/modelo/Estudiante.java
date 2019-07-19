1540adb9ed0ed8caac07d5363924f3e7b4d26c38
Christian <christianespinoza935@gmail.com>
2019-07-19 15:35:52 -0500

    Creacion Atributos Estudiante

--- TareaJPA/src/main/java/TareaJPA/modelo/Estudiante.java
+++ TareaJPA/src/main/java/TareaJPA/modelo/Estudiante.java
@@ -1,5 +1,131 @@
 package TareaJPA.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.ups.model.Materia;
+
 public class Estudiante {
+	
+	@Id
+	@Column(name="alum_codigo")
+	private int codigo;
+	
+	@NotNull
+	@Column(name="alum_nombre")
+	@Size(min = 3, max = 25)
+	private String nombre;
+	
+	@NotNull
+	@Column(name="alum_apellido")
+	@Size(min = 3, max = 25)
+	private String apellido;
+	
+	@NotNull
+	@Column(name="alum_cedula")
+	//@Size(min = 3, max = 25)
+	private String cedula;
+	
+	/*@NotNull
+    @Size(min = 9, max = 10)
+    @Digits(fraction = 0, integer = 10)*/
+	@Column(name = "alum_telefonor")
+	private String telefono;
+    
+	/*@NotNull
+	@NotEmpty
+	@Email*/
+	@Column(name="alum_email")
+	private String email;
+		
+	@NotNull
+	@Column(name="alum_direccion")
+	@Size(min = 3, max = 75)
+	private String direccion;
+
+	/*@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name="alum_codigo")
+	private List<Materia> materias;*/
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	
+	/*public List<Materia> getMaterias() {
+		return materias;
+	}
+
+	public void setMaterias(List<Materia> materias) {
+		this.materias = materias;
+	}
+
+	
+	public void addMateria(Materia mat) {
+		if(materias==null) {
+			materias = new ArrayList<>();
+		}
+		this.materias.add(mat);
+	}*/
 
 }
